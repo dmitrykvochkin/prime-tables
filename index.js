07a78be0ed88e6a141eea77645f7a245622c7fe0
Dmitry Kvochkin <dmitry@statustoday.com>
2018-11-01 20:23:51 +0000

    main script - passing the arguments

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+const argv = require('minimist')(process.argv.slice(2));
+const lib = require('./lib');
+
+if(
+    argv['help']
+    || (!argv['n']) 
+){  
+    console.log('\n###### Please use: ######');
+    console.log('node index.js --n=N');
+    process.exit(0);
+    return;
+}
+
+const n = argv['n'];
+
+console.log(`Calling primes table with n=${n}`);
